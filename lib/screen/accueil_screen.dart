6d97868c5bf247ed59fe9ce2808dcfd75aabb3c9
Soudaiss <soudaissisidk@groupeisi.com>
2025-03-09 14:38:09 +0000

    "premier commit"

---
+++ lib/screen/accueil_screen.dart
@@ -0,0 +1,67 @@
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
+
+class AccueilPage extends StatelessWidget {
+  const AccueilPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue.shade50,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Lottie.asset(
+              'assets/animations/weather.json',
+              width: 200,
+              height: 200,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Bienvenue dans WeatherApp!',
+              style: GoogleFonts.poppins(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue.shade900,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Obtenez les prévisions météo en un clic',
+              style: GoogleFonts.poppins(
+                fontSize: 16,
+                color: Colors.blueGrey,
+              ),
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue.shade700,
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 30, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+              onPressed: () {
+                Navigator.pushNamed(context, '/home'); // Redirige vers l’écran principal
+              },
+              child: Text(
+                'Commencer',
+                style: GoogleFonts.poppins(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
